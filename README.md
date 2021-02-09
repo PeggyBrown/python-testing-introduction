171f634166acd00b0428640d485ae4448f68a63a
PeggyBrown <ola.qnysz@gmail.com>
2021-02-09 11:39:45 +0100

    Added PriceCalculator.py with tests

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # python-testing-introduction
-Python testing introduction repo
+Repository used to demonstrate the process of writing tests in python  
+* PriceCalculator - allows getting gross prices based on provided vat (or default one)
+
+#To run the tests written in pytest
+use `python -m pytest TestPriceCalculator.py -vvv`  
+or just
+`python -m pytest TestPriceCalculator.py`
+if you don't need to see the details of tests being run  
+if you don't have `pytest`, simply run `pip install pytest`
+
---
+++ TestPriceCalculator.py
@@ -0,0 +1,24 @@
+import unittest
+
+import pytest
+
+from PriceCalculator import PriceCalculator
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_calculating_gross_price_with_defailt_vat(self):
+        calc = PriceCalculator()
+        result = calc.calculate_gross_price(100)
+        assert result == 123
+
+    def test_calculating_price_with_different_vat(self):
+        calc = PriceCalculator()
+        calc.change_vat(0.08)
+        result = calc.calculate_gross_price(100)
+        assert result == 108
+
+    def test_changing_vat_with_incorrect_value(self):
+        calc = PriceCalculator()
+        with pytest.raises(ValueError):
+            calc.change_vat(1)
